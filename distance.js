e8af85b1a4c79aa31d5f48fe98c16de1c25a8d6b
luespinal03 <luisespinal294@gmail.com>
2022-05-20 13:58:38 -0400

    Distance project

--- distance.js
+++ distance.js
@@ -2,6 +2,12 @@ let x1 = 1;
 let x2 = 8; 
 let y1 = 3; 
 let y2 = 10;
-let result=  9.8994; 
+let sum1 = x2 - x1; 
+let sum2 = y2 - y1; 
+let sqSum1 = sum1 * sum1; 
+let sqSum2 = sum2 * sum2; 
+let addSqSum = sqSum1 + sqSum2; 
+let distance = Math.sqrt(addSqSum)
+console.log(distance); 
 
-console.log("The distance between coordinates " + x1 + "," + y1 + " and " + x2 + "," + y2 + " is " + result);
+
