fdcecdea699467fd4069fc1cd4bab0a8d4b04b03
luespinal03 <luisespinal294@gmail.com>
2022-05-20 13:25:16 -0400

    Greeter project

---
+++ distance.js
@@ -0,0 +1,7 @@
+let x1 = 1; 
+let x2 = 8; 
+let y1 = 3; 
+let y2 = 10;
+let result=  9.8994; 
+
+console.log("The distance between coordinates " + x1 + "," + y1 + " and " + x2 + "," + y2 + " is " + result);
