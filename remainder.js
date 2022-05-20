1e995387efc6fdabd6f03a670497c88d2d079eed
luespinal03 <luisespinal294@gmail.com>
2022-05-20 13:30:53 -0400

    Remainder project

--- remainder.js
+++ remainder.js
@@ -1,5 +1,4 @@
 let num1 = 2;
 let num2 = 4; 
-let num3 = num2%num1; 
-
-console.log("The remainder of " + num2 + " divided by " + num1 +  " equals to " + num3 ); 
+// let num3 = num2%num1; 
+console.log("The remainder of " + num2 + " divided by " + num1 +  " equals to " + num2 % num1); 
