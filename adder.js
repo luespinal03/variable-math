0734af40951a872094b71aba1681d0e951ecbe92
luespinal03 <luisespinal294@gmail.com>
2022-05-23 10:47:55 -0400

    Updated js code

--- adder.js
+++ adder.js
@@ -1,6 +1,6 @@
 let num1 = 10; 
 let num2 = 5; 
 
-let num3 = num1 + num2; 
+let sum = num1 + num2; 
 
-console.log(num1 + " and " + num2 + " added together equals to " + num3);
+console.log(num1 + " and " + num2 + " added together equals to " + sum);
